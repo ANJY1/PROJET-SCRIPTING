4cff9b122b5ee996866ab983a982f1eb52c87e2a
mulgan25ngougbia <47834871+mulgan25ngougbia@users.noreply.github.com>
2020-03-06 14:32:15 +0100

    Create erreur.sh

---
+++ erreur.sh
@@ -0,0 +1,12 @@
+# PROJET-SCRIPTING
+Projet réalisé en langage shell 
+ouvrirFichier()
+{
+./main.sh
+}
+
+
+yad --title="erreur de commande"  --width="400" --height="200" --button="OK":1\
+ --text-info --wrap --back=black --fore=red --justify=fill < erreur.txt
+ouvrirFichier
+
