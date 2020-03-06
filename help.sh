822a1d900cd6731b4fd7872b2c45c5005e2fa4e8
mulgan25ngougbia <47834871+mulgan25ngougbia@users.noreply.github.com>
2020-03-06 14:33:43 +0100

    Create help.sh

---
+++ help.sh
@@ -0,0 +1,12 @@
+# PROJET-SCRIPTING
+Projet réalisé en langage shell 
+#! /bin/bash
+
+source programme.sh
+
+yad --title=$1 --text-align=center --width="800" --height="200" --button="OK":1 \
+ --text-info --back=white --fore=blue < help.txt 
+ouvrirFichier
+
+
+ouvrirchoix
