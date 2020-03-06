32e3c2543c2fe2ce9cf797e65d65bbffdac8c2d3
mulgan25ngougbia <47834871+mulgan25ngougbia@users.noreply.github.com>
2020-03-06 14:38:48 +0100

    Create programme.sh

---
+++ programme.sh
@@ -0,0 +1,26 @@
+# PROJET-SCRIPTING
+Projet réalisé en langage shell 
+#! /bin/bash
+
+ouvrirFichier()
+{
+choix=(/home/$USER/Desktop/projet_scriptingF)
+cd $choix
+./main.sh
+}
+
+ouvrirchoix()
+{
+choix=(/home/$USER/Desktop/projet_scriptingF)
+cd $choix
+./choix.sh
+}
+ouvrirretour()
+{
+choix=(/home/$USER/Desktop/projet_scriptingF)
+cd $choix
+./
+}
+
+
+
